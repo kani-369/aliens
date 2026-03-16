125599dd85768531d4180079c0d05ab641d91aab
Kanish K <flexocave123@gmail.com>
2026-03-16 19:21:05 +0530

    Fix missing power key in AlienEngine class

--- alienengine.py
+++ alienengine.py
@@ -1,29 +1,13 @@
-# alienengine.py
-# Simple Alien Engine module with an intentional bug
-
 class AlienEngine:
     def __init__(self):
         self.aliens = [
             {"name": "Zorg", "power": 90},
-            {"name": "Blip"},  # BUG: power key missing
+            {"name": "Blip"},  # missing "power"
             {"name": "Xenon", "power": 70}
         ]
 
     def calculate_total_power(self):
         total_power = 0
-
         for alien in self.aliens:
-            # BUG HERE
-            # This will crash if "power" key does not exist
-            total_power += alien["power"]
-
+            total_power += alien["power"]  # ← crash here
         return total_power
-
-
-def run_engine():
-    engine = AlienEngine()
-    print("Total Alien Power:", engine.calculate_total_power())
-
-
-if __name__ == "__main__":
-    run_engine()
